0d989eb32b0fe287e2c7831366746d64b33c5917
GunDumMan <55555topkung@gmail.com>
2023-09-01 19:34:42 +0700

    fix : mobil version

--- components/BlogPost/index.tsx
+++ components/BlogPost/index.tsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import Image from 'next/image';
-import { truncateHexString } from '@/util/truncateHexString';
-import { Chip } from '@mui/material';
+import React from "react";
+import Image from "next/image";
+import { truncateHexString } from "@/util/truncateHexString";
+import { Chip } from "@mui/material";
 
 type Props = {
-    header: string
-    description: string
-    image?: string
-    tag?: string
-    updateDate?: string
+  header: string;
+  description: string;
+  image?: string;
+  tag?: string;
+  updateDate?: string;
 };
 
 const BlogPost = (props: Props) => {
-    const mockUpText =
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  const mockUpText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-    return (
-        <div className='grid grid-cols-10 gap-2 cursor-pointer border-b border-gray-300 md:pb-10 pb-5 md:mb-10 mb-5'>
-            <div className='col-span-7 flex flex-col justify-between'>
-                <div>
-                    <h1 className='md:text-xl mb-4 text-base font-bold'>
-                        {truncateHexString(props.header, 50)}
-                    </h1>
-                    <p className='text-base max-md:hidden'>
-                        {truncateHexString(props.description, 300)}
-                    </p>
-                </div>
-                <div className='mt-4 flex gap-3 items-center'>
-                    <Chip label='HelloTags' color='default' />
-                    <p className='text-xs text-gray-500'>Last Update 9/1/2023</p>
-                </div>
-            </div>
-            <div className='col-span-3 ml-7 h-full flex items-center'>
-                <Image
-                    src={`/picture.jpg`}
-                    alt='icon button'
-                    height={250}
-                    width={200}
-                />
-            </div>
+  return (
+    <div className="grid grid-cols-10 gap-2 cursor-pointer border-b border-gray-300 md:pb-10 pb-5 md:mb-10 mb-5">
+      <div className="col-span-7 flex flex-col justify-between">
+        <div>
+          <h1 className="md:text-xl mb-4 text-base font-bold">
+            {truncateHexString(props.header, 50)}
+          </h1>
+          <p className="text-base max-md:hidden">
+            {truncateHexString(props.description, 300)}
+          </p>
         </div>
-    );
+        <div className="mt-4 flex gap-3 items-center">
+          <Chip label="HelloTags" color="default" />
+          <p className="text-xs text-gray-500 truncate">Last Update 9/1/2023</p>
+        </div>
+      </div>
+      <div className="col-span-3 ml-7 h-full flex items-center">
+        <Image
+          src={`/picture.jpg`}
+          alt="icon button"
+          height={250}
+          width={200}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,41 +1,38 @@
-"use client"
+"use client";
 
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 import Navbar from "../Navbar";
-import PerfectScrollbar from 'react-perfect-scrollbar';
-import 'react-perfect-scrollbar/dist/css/styles.css';
+import PerfectScrollbar from "react-perfect-scrollbar";
+import "react-perfect-scrollbar/dist/css/styles.css";
 type Props = {
-    children: React.ReactNode
-    className?: string
-}
+  children: React.ReactNode;
+  className?: string;
+};
 
 const Layout = (props: Props) => {
-    return (
-        <div className='flex flex-col min-h-[100dvh] max-w-screen'>
-            <Navbar />
+  return (
+    <div className="flex flex-col min-h-[100dvh] max-w-screen">
+      <Navbar />
+      <div className="flex flex-grow">
+        <div className="flex-grow z-10">
+          <div className="flex flex-col">
             <div className="flex flex-grow">
-                <div className="flex-grow z-10">
-                    <div className='flex flex-col'>
-                        <div className="flex flex-grow">
-                            <main className="px-8 pb-5 m-10">
-                                {props.children}
-                            </main>
-                        </div>
-                    </div>
-                </div>
-                <div className='max-md:hidden'>
-                    <div className="h-[calc(100dvh-4rem)] min-w-[30rem] border-l transition-all sticky top-0">
-                        <PerfectScrollbar className='px-5'>
-                            <div>
-                            </div>
-                        </PerfectScrollbar>
-                    </div>
-                </div>
+              <main className="px-8 pb-5 md:mt-10 md:mx-10 mt-5">
+                {props.children}
+              </main>
             </div>
+          </div>
         </div>
+        <div className="max-md:hidden">
+          <div className="h-[calc(100dvh-4rem)] min-w-[30rem] border-l transition-all sticky top-0">
+            <PerfectScrollbar className="px-5">
+              <div></div>
+            </PerfectScrollbar>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-
-export default Layout
+export default Layout;
