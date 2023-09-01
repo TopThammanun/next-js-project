90fb39a13b6951e5c11b11dc95826952351a47c9
Top <thammanun_a@cmu.ac.th>
2023-09-01 14:57:51 +0700

    fix : margin bottom post

--- components/BlogPost/index.tsx
+++ components/BlogPost/index.tsx
@@ -14,7 +14,7 @@ const BlogPost = (props: Props) => {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
     return (
-        <div className='grid grid-cols-10 gap-2 cursor-pointer border-b border-gray-300 pb-10 mb-10'>
+        <div className='grid grid-cols-10 gap-2 cursor-pointer border-b border-gray-300 pb-10 md:mb-10 mb-5'>
             <div className='col-span-7 flex flex-col justify-between'>
                 <div>
                     <h1 className='md:text-xl mb-4 text-base font-bold'>
