6e6ffbad870629251d5ec75f23555faf57e75d54
Top <thammanun_a@cmu.ac.th>
2023-09-04 12:57:45 +0700

    fix : splite nav bar

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import React, { Fragment } from "react";
 import Navbar from "../Navbar";
-import PerfectScrollbar from "react-perfect-scrollbar";
-import "react-perfect-scrollbar/dist/css/styles.css";
+import SlideBar from "../SlideBar";
 type Props = {
   children: React.ReactNode;
   className?: string;
@@ -17,7 +14,7 @@ const Layout = (props: Props) => {
         <div className="flex-grow z-10">
           <div className="flex flex-col">
             <div className="flex flex-grow">
-              <main className="px-8 pb-5 md:mt-10 md:mx-10 mt-5">
+              <main className="px-8 pb-5 md:mt-10 lg:mx-72 mt-5">
                 {props.children}
               </main>
             </div>
@@ -25,64 +22,7 @@ const Layout = (props: Props) => {
         </div>
         <div className="max-md:hidden">
           <div className="h-[100dvh] min-w-[25rem] border-l transition-all sticky top-0">
-            <PerfectScrollbar className="px-5 pt-6">
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-              <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
-            </PerfectScrollbar>
+            <SlideBar />
           </div>
         </div>
       </div>
---
+++ components/SlideBar/index.tsx
@@ -0,0 +1,67 @@
+import PerfectScrollbar from "react-perfect-scrollbar";
+import "react-perfect-scrollbar/dist/css/styles.css";
+
+const SlideBar = () => {
+    return (
+        <PerfectScrollbar className="px-5 pt-6">
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+            <div className="bg-gray-200 rounded-xl p-3 m-2">Hello</div>
+        </PerfectScrollbar>
+    );
+}
+
+export default SlideBar;
